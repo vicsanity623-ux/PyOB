9234d7f9ff6cd1a2a0ea6e649d4e1f5b5103ed2d
Vic <125237471+vicsanity623@users.noreply.github.com>
2026-03-09 01:22:12 -0700

    Update pyob_dashboard.py

--- src/pyob/pyob_dashboard.py
+++ src/pyob/pyob_dashboard.py
@@ -1,7 +1,7 @@
 import json
 import os
 from http.server import BaseHTTPRequestHandler
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any
 
 if TYPE_CHECKING:
     from pyob.entrance import EntranceController
